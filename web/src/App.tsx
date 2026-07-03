479e0ef3731817fb74c0d9b573eb8defbce0feb2
Harshit <harshit.king2003@gmail.com>
2026-07-03 08:41:39 +0530

    First Commit

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+export default function App(){
+  return (
+    <div className="min-h-screen bg-black flex flex-col items-center justify-center">
+      <h1 className="text-7xl font-extrabold text-white">
+        Drishya
+        </h1>
+      <p className="mt-4 text-gray-400 text-xl">
+        An Indian Social Media Platform
+        </p>
+    </div>
+  );
+}
